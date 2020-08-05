b446eeeea7942483119434a65da3483f3dc55c00
kemosaf <63350810+kemosaf@users.noreply.github.com>
2020-08-05 08:10:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let buttonClick = new Audio()
-buttonClick.src = "./assets/buttonClick.mp3";
+buttonClick.src = "./audio/assets/buttonClick.mp3";
 
  buttonClick.play()
  const button = document.getElementById("invite_Button")
